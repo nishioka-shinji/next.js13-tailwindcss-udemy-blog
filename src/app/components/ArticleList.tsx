4e2df3d54a488986af527bbb99bc25121fbce84d
nishioka-shinji <s61nov11.shinji.nishioka@gmail.com>
2023-10-01 07:44:23 +0900

    APIで取得して作成

--- src/app/components/ArticleList.tsx
+++ src/app/components/ArticleList.tsx
@@ -14,7 +14,7 @@ const ArticleList = ({ articles }: ArticleListProps) => {
         <article key={article.id} className="shadow my-4 flex flex-col">
           <Link href={`articles/${article.id}`} className="hover:opacity-75">
             <Image
-              src="https://source.unsplash.com/collection/1346951/1000x500?sig=1"
+              src={`https://source.unsplash.com/collection/1346951/1000x500?sig=${article.id}}`}
               width={1280}
               height={300}
               alt="nextjs"
--- src/blogAPI.ts
+++ src/blogAPI.ts
@@ -1,4 +1,5 @@
 import { Article } from "./types";
+import { notFound } from "next/navigation";
 
 export const getAllArticles = async (): Promise<Article[]> => {
   const res = await fetch(`http://localhost:3001/posts`, { cache: "no-store" }); //SSR
@@ -7,6 +8,27 @@ export const getAllArticles = async (): Promise<Article[]> => {
     throw new Error("Error from getAllArticles");
   }
 
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+
   const articles = await res.json();
   return articles;
+};
+
+export const getDetailArticle = async (id: string): Promise<Article> => {
+  const res = await fetch(`http://localhost:3001/posts/${id}`, {
+    next: { revalidate: 60 },
+  }); //ISR
+
+  if (res.status === 404) {
+    notFound();
+  }
+
+  if (!res.ok) {
+    throw new Error("Error from getAllArticles");
+  }
+
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+
+  const article = await res.json();
+  return article;
 };
